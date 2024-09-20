14f59c76c1246f431836a9feee647273af1792ab
Clrain_Laptop <jin_hyeon_seo_01@naver.com>
2024-09-20 10:49:27 +0900

    Update Win_IOCP.cpp

--- Win_IOCP/Win_IOCP.cpp
+++ Win_IOCP/Win_IOCP.cpp
@@ -2,16 +2,24 @@
 
 int main(int argc, char* argv[])
 {
-	// 윈속 초기화
 	WSADATA data;
 	if (WSAStartup(MAKEWORD(2, 2), &data) != 0)
-		err_quit("Error");
-	std::cout << "열림\n";
+		return 0;
+
+	struct sockaddr_in sockAddress;
+	std::memset(&sockAddress, 0, sizeof(sockAddress));
+
+	inet_pton(AF_INET, "127.0.0.1", &sockAddress.sin_addr);
+	sockAddress.sin_port = htons(56671);
+
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (sock == INVALID_SOCKET)
-		err_quit("Error");
-	std::cout << "닫음\n";
+	if (sock != 0)
+		err_quit("ERRPR : ");
+
+	int retval = connect(sock, (struct sockaddr*)&sockAddress, sizeof(sockAddress));
+	if (retval == SOCKET_ERROR) err_quit("connect()");
 	closesocket(sock);
+
 	WSACleanup();
 	return 0;
 }
