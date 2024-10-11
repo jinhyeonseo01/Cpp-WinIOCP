c593cf78c509e43503d30aa2385f52180cf362fe
Clrain_Laptop <jin_hyeon_seo_01@naver.com>
2024-10-11 11:05:07 +0900

    123

--- Study_Server/Study_Server.cpp
+++ Study_Server/Study_Server.cpp
@@ -1,114 +1,110 @@
 ﻿#include "Common.h"
 
-#include <iostream>
-#include <array>
-#include <vector>
-#include <algorithm>
-#include <deque>
-#include <fstream>
-#include <list>
-#include <memory>
-#include <ranges>
-#include <functional>
-#include <numbers>
-#include <numeric>
-#include <string>
-#include <string_view>
+#define SERVERPORT 9000
+#define BUFSIZE    512
 
-
-void clearConsole() {
-#ifdef _WIN32
-	system("cls");
-#else
-	system("clear");
-#endif
-}
-
-
-int main(int argc, char* argv[])
+// 클라이언트와 데이터 통신
+DWORD WINAPI ProcessClient(LPVOID arg)
 {
-	WSADATA data;
-	if (WSAStartup(MAKEWORD(2, 2), &data) != 0)
-		return 0;
-
-	sockaddr_in addr;
-	memset(&addr, 0, sizeof(addr));
-	//inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
-	addr.sin_addr.s_addr = 0;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(9000);
-
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-	int a = bind(sock, (sockaddr*)&addr, sizeof(addr));
-	int b = listen(sock, 10);
-
-	SOCKET client_sock;
+	int retval;
+	SOCKET client_sock = (SOCKET)(arg);
 	struct sockaddr_in clientaddr;
+	char addr[INET_ADDRSTRLEN];
 	int addrlen;
+	char buf[BUFSIZE + 1];
 
+	// 클라이언트 정보 얻기
 	addrlen = sizeof(clientaddr);
-	client_sock = accept(sock, (struct sockaddr*)&clientaddr, &addrlen);
-	if (client_sock == INVALID_SOCKET) {
-		err_display("accept()");
-	}
-	printf("Connect!\n");
+	getpeername(client_sock, (struct sockaddr*)&clientaddr, &addrlen);
+	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
 
-	unsigned long long int fileSize;
-	int retval = recv(client_sock, (char*)(&fileSize), 8, MSG_WAITALL);
-	fileSize = ntohll(fileSize);
-
-	unsigned short fileNameSize;
-	retval = recv(client_sock, (char*)(&fileNameSize), 2, MSG_WAITALL);
-	fileNameSize = ntohs(fileNameSize);
-
-	printf("fileSize : %lld\n", static_cast<long long int>(fileSize));
-	std::vector<char> nameData;
-	nameData.resize((fileNameSize+1));
-	retval = recv(client_sock, (char*)nameData.data(), static_cast<short>(fileNameSize), MSG_WAITALL);
-	nameData[fileNameSize] = '\0';
-	std::string fileName = nameData.data();
-	std::cout << "fileName : " << fileName<<"\n";
-	clearConsole();
-	COORD coord;
-	coord.X = 0;
-	coord.Y = 0;
-
-	std::deque<char> fileData;
-	std::array<char, 1024> buf;
-	int offset = 0;
 	while (1) {
-		int readSize = min(buf.size(), fileSize - offset);
-		if (readSize <= 0)
-			break;
-		int retval = recv(client_sock, buf.data(), readSize, MSG_WAITALL);
+		// 데이터 받기
+		retval = recv(client_sock, buf, BUFSIZE, 0);
 		if (retval == SOCKET_ERROR) {
 			err_display("recv()");
 			break;
 		}
 		else if (retval == 0)
 			break;
-		for (int i = 0; i < retval; i++)
-			fileData.push_back(buf[i]);
-		offset += retval;
 
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-		printf("수신률 : %lf%%                \n", (offset / (float)fileSize)*100);
+		// 받은 데이터 출력
+		buf[retval] = '\0';
+		printf("[TCP/%s:%d] %s\n", addr, ntohs(clientaddr.sin_port), buf);
+
+		// 데이터 보내기
+		retval = send(client_sock, buf, retval, 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("send()");
+			break;
+		}
 	}
-	printf("수신완료\n");
-	printf("저장중\n");
 
-	std::ofstream os(fileName, std::ios::binary);
-
-	for (int i = 0; i < fileData.size(); i++)
-		os.write((char*)&fileData[i], 1);
-	os.close();
-	printf("저장완료\n");
-
-	scanf("%d");
-	//accept()
-
-	WSACleanup();
+	// 소켓 닫기
+	closesocket(client_sock);
+	printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
+		addr, ntohs(clientaddr.sin_port));
 	return 0;
 }
 
+int main(int argc, char* argv[])
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// 소켓 생성
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+
+	// bind()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
+
+	// listen()
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+
+	// 데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	struct sockaddr_in clientaddr;
+	int addrlen;
+	HANDLE hThread;
+
+	while (1) {
+		// accept()
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
+		if (client_sock == INVALID_SOCKET) {
+			err_display("accept()");
+			break;
+		}
+
+		// 접속한 클라이언트 정보 출력
+		char addr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+			addr, ntohs(clientaddr.sin_port));
+
+		// 스레드 생성
+		hThread = CreateThread(NULL, 0, ProcessClient,
+			(LPVOID)client_sock, 0, NULL);
+		if (hThread == NULL) { closesocket(client_sock); }
+		else { CloseHandle(hThread); }
+	}
+
+	// 소켓 닫기
+	closesocket(listen_sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
--- TCPClient/Study_Client.cpp
+++ TCPClient/Study_Client.cpp
@@ -1,95 +1,15 @@
-#include "../Win_IOCP/Common.h"
+#include "Common.h"
 
-
-#define _CRT_SECURE_NO_WARNINGS // 구형 C 함수 사용 시 경고 끄기
-#define _WINSOCK_DEPRECATED_NO_WARNINGS // 구형 소켓 API 사용 시 경고 끄기
-
-#include <deque>
-#include <fstream>
-#include <winsock2.h> // 윈속2 메인 헤더
-#include <ws2tcpip.h> // 윈속2 확장 헤더
-
-#include <tchar.h> // _T(), ...
-#include <stdio.h> // printf(), ...
-#include <stdlib.h> // exit(), ...
-#include <string.h> // strncpy(), ...
-#include <iostream>
-#include <list>
-#include <vector>
-
-#pragma comment(lib, "ws2_32") // ws2_32.lib 링크
-
-char *SERVERIP = (char *)"127.0.0.1";
-std::string fileName = "";
+char* SERVERIP = (char*)"127.0.0.1";
 #define SERVERPORT 9000
 #define BUFSIZE    512
 
-
-
-void clearConsole() {
-#ifdef _WIN32
-	system("cls");
-#else
-	system("clear");
-#endif
-}
-
-
-int Test(void* arg)
-{
-
-	return 0;
-}
-
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	int retval;
 
-	CreateThread(0,0, Test,0,0, 0);
-
-
-
 	// 명령행 인수가 있으면 IP 주소로 사용
-	if (argc > 2)
-	{
-		SERVERIP = argv[1];
-		fileName = std::string(argv[2], argv[2]+strlen(argv[2]));
-	}
-	printf("%d\n", fileName.size());
-	printf("파일 읽는 중 %s\n", fileName.c_str());
-	std::ifstream is{ fileName, std::ios::binary};
-	std::deque<char> fileData;
-	if (!is)
-	{
-		printf("파일 읽기 실패\n");
-		return 0;
-	}
-
-	int headerSize = 8;
-
-	is.seekg(0, std::ios::end);
-	long long fileSize = is.tellg();
-	is.seekg(0, std::ios::beg);
-	unsigned long long int networkFileSize = htonll(static_cast<unsigned long long int>(fileSize));//
-	unsigned short networkFileNameSize = htons(static_cast<short>(fileName.size()));//
-	for(int i=0;i< headerSize;i++)
-		fileData.emplace_back(((char*)&networkFileSize)[i]);
-
-	for (int i = 0; i < 2; i++)
-		fileData.emplace_back(((char*)&networkFileNameSize)[i]);
-
-	for (int i = 0; i < fileName.size(); i++)
-		fileData.emplace_back(fileName.data()[i]);
-
-
-	char buffer;
-	while (is.read(&buffer, sizeof(buffer))) {
-		fileData.push_back(buffer); // deque에 추가
-	}
-
-	printf("파일 읽기 성공 : 파일 크기(%d)\n", fileSize);
-
+	if (argc > 1) SERVERIP = argv[1];
 
 	// 윈속 초기화
 	WSADATA wsa;
@@ -103,44 +23,52 @@ int main(int argc, char *argv[])
 	// connect()
 	struct sockaddr_in serveraddr;
 	memset(&serveraddr, 0, sizeof(serveraddr));
-	inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr);
 	serveraddr.sin_family = AF_INET;
+	inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr);
 	serveraddr.sin_port = htons(SERVERPORT);
-
-
-	retval = connect(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+	retval = connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
 	if (retval == SOCKET_ERROR) err_quit("connect()");
 
-	printf("Connect!\n");
-	clearConsole();
-	COORD coord;
-	coord.X = 0;
-	coord.Y = 0;
+	// 데이터 통신에 사용할 변수
+	char buf[BUFSIZE + 1];
+	int len;
 
-	std::array<char, 1024> packetData;
-	int offset = 0;
+	// 서버와 데이터 통신
 	while (1) {
 		// 데이터 입력
-		int size = min(packetData.size(), fileData.size() - offset);
-		if(size <= 0)
+		printf("\n[보낼 데이터] ");
+		if (fgets(buf, BUFSIZE + 1, stdin) == NULL)
 			break;
-		std::copy(fileData.begin() + offset, fileData.begin() + offset + size, packetData.data());
+
+		// '\n' 문자 제거
+		len = (int)strlen(buf);
+		if (buf[len - 1] == '\n')
+			buf[len - 1] = '\0';
+		if (strlen(buf) == 0)
+			break;
+
 		// 데이터 보내기
-		retval = send(sock, packetData.data(), size, 0);
+		retval = send(sock, buf, (int)strlen(buf), 0);
 		if (retval == SOCKET_ERROR) {
 			err_display("send()");
 			break;
 		}
-		if(retval == 0)
-		{
-			err_display("result = 0");
+		printf("[TCP 클라이언트] %d바이트를 보냈습니다.\n", retval);
+
+		// 데이터 받기
+		retval = recv(sock, buf, retval, MSG_WAITALL);
+		if (retval == SOCKET_ERROR) {
+			err_display("recv()");
+			break;
 		}
-		offset += retval;
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-		printf("전송중 : %lf%%                \n", offset/(float)(fileSize+headerSize) * 100);
-		//clearConsole();
+		else if (retval == 0)
+			break;
+
+		// 받은 데이터 출력
+		buf[retval] = '\0';
+		printf("[TCP 클라이언트] %d바이트를 받았습니다.\n", retval);
+		printf("[받은 데이터] %s\n", buf);
 	}
-	printf("전송완료\n");
 
 	// 소켓 닫기
 	closesocket(sock);
@@ -149,4 +77,3 @@ int main(int argc, char *argv[])
 	WSACleanup();
 	return 0;
 }
-
